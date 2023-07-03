493d4f6067ab23b41e52fa83fe21ea12a7d684f8
Christopher Raven <c.raven@me.com>
2023-07-03 13:37:42 +0100

    Initial Commit

---
+++ breathe.assitant/breathe_assitantApp.swift
@@ -0,0 +1,17 @@
+//
+//  breathe_assitantApp.swift
+//  breathe.assitant
+//
+//  Created by Christopher Raven on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct breathe_assitantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
